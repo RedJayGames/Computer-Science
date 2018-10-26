105330654c76024cb758be9a52a3e814150e4eef
Corbin Grosso <corby81019@gmail.com>
2018-10-26 11:26:19 -0400

    Revert "Completed the Maze"
    This reverts commit c8ae410321936ea9e2225ef527f11eb083f3eff8.

--- Free Time/src/Dots.java
+++ Free Time/src/Dots.java
@@ -3,13 +3,13 @@ import javax.swing.*;
 public class Dots {
 
 	public static void main(String[] args) {
-//		JFrame dotsFrame = new JFrame ("Dots");
-//		dotsFrame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-//		
-//		dotsFrame.getContentPane().add(new DotsPanel());
-//		
-//		dotsFrame.pack();
-//		dotsFrame.setVisible(true);
+		JFrame dotsFrame = new JFrame ("Dots");
+		dotsFrame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		
+		dotsFrame.getContentPane().add(new DotsPanel());
+		
+		dotsFrame.pack();
+		dotsFrame.setVisible(true);
 
 	}
 
--- Free Time/src/MazeGame.java
+++ Free Time/src/MazeGame.java
@@ -14,6 +14,7 @@ public class MazeGame extends Applet {
 		wallsY = new int[1444];
 		pathX = new int[1444];
 		pathY = new int[1444];
+<<<<<<< HEAD
 
 
 		setBackground (Color.white);
@@ -112,12 +113,79 @@ public class MazeGame extends Applet {
 			}
 		}
 
+=======
+		
+		
+		setBackground (Color.white);
+
+		Random rand = new Random();
+		Scanner scan = new Scanner(System.in);
+		//fills border
+		g.setColor(Color.black);
+		g.fillRect(0, 0, 5, 200);
+		g.fillRect(0, 0, 200, 5);
+		g.fillRect(195, 5, 10, 200);
+		g.fillRect(5, 195, 200, 10);
+
+		//creates path
+		for(int i = 0, determine;!(x == 190 && y == 190);i++){
+			determine = rand.nextInt(14);
+			if(determine < 3 && determine >= 0 && x >= 10){
+				x-=5;
+				g.setColor(Color.white);
+				g.fillRect(x, y, 5, 5);
+			}else if(determine < 6 && determine >= 3 && y >= 10){
+				y-=5;
+				g.setColor(Color.white);
+				g.fillRect(x, y, 5, 5);
+			}else if(determine < 10 && determine >= 6 && x <= 185){
+				x+=5;
+				g.setColor(Color.white);
+				g.fillRect(x, y, 5, 5);
+			}else if(determine >= 10 && y<=185){
+				y+=5;
+				g.setColor(Color.white);
+				g.fillRect(x, y, 5, 5);
+			}
+			pathX[i] = x;
+			pathY[i] = y;
+
+			//reduces length of array
+			if((x == 185 && y == 190) || (x == 190 && y == 185)) {
+			pathX1 = Arrays.copyOf(pathX, i);
+			pathY1 = Arrays.copyOf(pathY, i);
+			}
+		}
+		
+		//creates walls in random locations
+		for(int wallx, wally, i = 0, count = 1000; count > 0; i++){
+			g.setColor(Color.black);
+			wallx = (rand.nextInt(38) + 1) * 5;
+			wally = (rand.nextInt(38) + 1) * 5;
+			g.fillRect(wallx, wally, 5, 5);
+			wallsX[i] = wallx;
+			wallsY[i] = wally;
+			
+			count--;
+			
+			//reduces length of array
+			if(count == 0) {
+			wallsX1 = Arrays.copyOf(wallsX, i);
+			wallsY1 = Arrays.copyOf(wallsY, i);
+			}
+		}
+		
+>>>>>>> parent of c8ae410... Completed the Maze
 		//re-color path
 		for(int i = 0;i < pathX1.length;i++) {
 			g.setColor(Color.white);
 			g.fillRect(pathX1[i], pathY1[i], 5, 5);
 		}
+<<<<<<< HEAD
 
+=======
+		
+>>>>>>> parent of c8ae410... Completed the Maze
 		//remove walls from path
 		for(int i = 0;i < pathX1.length && i < wallsX1.length; i++){
 			for(int j = 0; j < pathX1.length && j < wallsX1.length; j++){
@@ -127,21 +195,37 @@ public class MazeGame extends Applet {
 				}
 			}
 		}
+<<<<<<< HEAD
 
 		//reset variables
 		x = 5;
 		y = 5;
 
+=======
+		
+		//reset variables
+		x = 5;
+		y = 5;
+		
+>>>>>>> parent of c8ae410... Completed the Maze
 		//ensures starting location
 		g.setColor(Color.white);
 		g.fillRect(5, 5, 5, 5);
 		g.fillRect(5, 10, 5, 5);
 		g.fillRect(10, 5, 5, 5);
+<<<<<<< HEAD
 
 		//shows exit to maze
 		g.setColor(Color.magenta);
 		g.fillRect(190, 190, 5, 5);
 
+=======
+		
+		//shows exit to maze
+		g.setColor(Color.magenta);
+		g.fillRect(190, 190, 5, 5);
+		
+>>>>>>> parent of c8ae410... Completed the Maze
 		//allows player movement
 		for(String input;win == false;){
 			wall = false;
